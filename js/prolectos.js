c547bace011a60ac49b38fee97c3f2f60b7e92af
SCTech-MZ <silviocustodiotech@gmail.com>
2026-08-18 11:02:56 +0200

    First commit

---
+++ js/prolectos.js
@@ -0,0 +1,20 @@
+// const projectsSection = document.getElementById("gradeProjectos");
+
+// let projectos = [
+//     {id:1,category: "Simple Landing page", target: "#",bussinessArea: "Real estate",description: "Plataform to convert visitors in clients",funcionaliyes:""}
+// ]
+
+// projectos.map((e) => {
+//     const div = document.createElement("div");
+//     div.setAttribute("class", "projecto");
+//     div.innerHTML = `
+//     <div class="img">ACX</div>
+//     <div class="infos">
+//     <h2>${e.category}</h2>
+//     <span>${e.bussinessArea}</span>
+//     <p>${e.description}</p>
+//     <div class="ctas">
+//         <a href="${e.target}" class="btn-ghost">See project</a>
+//     </div>`;
+//     projectsSection.appendChild(div)
+// })
